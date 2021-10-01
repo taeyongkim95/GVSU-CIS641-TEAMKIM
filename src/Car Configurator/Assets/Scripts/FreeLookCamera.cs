fd7b9de9041f95c0d253908716ac9faee8d6a319
taeyongkim95 <taeyongkim.95@gmail.com>
2021-09-30 23:25:37 -0400

    Made good progress

--- src/Car Configurator/Assets/Scripts/FreeLookCamera.cs
+++ src/Car Configurator/Assets/Scripts/FreeLookCamera.cs
@@ -6,7 +6,7 @@ using Cinemachine;
 public class FreeLookCamera : MonoBehaviour
 {
     [SerializeField] GameObject freeLookCamera;
-    CinemachineFreeLook freeLookComponent;
+    private CinemachineFreeLook freeLookComponent;
 
     private void Awake()
     {
@@ -15,28 +15,16 @@ public class FreeLookCamera : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(1))
         {
             // use the following line for mouse control of zoom instead of mouse wheel
-            // be sure to change Input Axis Name on the Y axis to "Mouse Y"
-
-            freeLookComponent.m_YAxis.m_MaxSpeed = 10;
-            freeLookComponent.m_XAxis.m_MaxSpeed = 400;
+            freeLookComponent.m_XAxis.m_MaxSpeed = 500;
         }
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(1))
         {
             // use the following line for mouse control of zoom instead of mouse wheel
             // be sure to change Input Axis Name on the Y axis from to "Mouse Y"
-
-            freeLookComponent.m_YAxis.m_MaxSpeed = 0;
             freeLookComponent.m_XAxis.m_MaxSpeed = 0;
-        }
-
-        // wheel zoom //
-        // comment out the below if condition if you are using mouse control for zoom
-        if (Input.mouseScrollDelta.y != 0)
-        {
-            freeLookComponent.m_YAxis.m_MaxSpeed = 10;
         }
     }
 }
