df4a14e41a4be336a085ace88eb01f1abe72b932
taeyongkim95 <taeyongkim.95@gmail.com>
2021-10-30 22:18:01 -0400

    Created DriveScene and created scene loading

--- src/Car Configurator/Assets/Scripts/ConfigScene/TestDriveButton.cs
+++ src/Car Configurator/Assets/Scripts/ConfigScene/TestDriveButton.cs
@@ -1,16 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class TestDriveButton : CustomButton
-{
+public class TestDriveButton : MonoBehaviour
+{ 
     void Start()
     {
-        
     }
 
     public void StartDrive()
     {
-        Debug.Log("ok");
-    }
+        SceneManager.LoadScene("DriveScene");
+    }    
 }
